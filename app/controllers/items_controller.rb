0e5643585e363ca9ce19db3df5c65cf1c6ef51a5
freedom884 <bass.ran.003@gmail.com>
2024-01-28 22:21:31 +0900

    出品のルーティング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(products_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def message_params
+    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: "items#index"
 
-  resources :items, only: :index
-  
-
-
- 
-
+  resources :items, only: [:index, :create, :new]
 end
