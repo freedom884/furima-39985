80dc918c350df24573614e4b1f476b9e9d1de786
freedom884 <bass.ran.003@gmail.com>
2024-02-07 02:04:56 +0900

    ログアウト状態で編集不可

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,13 +23,35 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
+    unless current_user.id == @product.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      @product.save
+    render :edit ,status: :unprocessable_entity
+    end
   end
   
+  def move_to_index
+    @product = Product.find(params[:id])
+    unless current_user.id == @product.user_id
+      redirect_to root_path
+    end
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:name, :description, :image,:category_id ,:condition_id,:shippingfee_id,:prefecture_id,:scheduleddelivery_id,:price).merge(user_id: current_user.id,)
   end
+
  
 
 end
