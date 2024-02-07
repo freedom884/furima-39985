9c25b81dd57031989853764919fe6ba7fcc686fb
freedom884 <bass.ran.003@gmail.com>
2024-02-07 15:25:08 +0900

    削除の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :set_product, only: [:show, :edit, :update, :move_to_index]
-  before_action :require_same_user, only: [:edit, :move_to_index]
+  before_action :set_product, only: [:show, :edit, :update, :move_to_index, :destroy]
+  before_action :require_same_user, only: [:edit, :move_to_index,:destroy]
 
 
   def index 
@@ -37,6 +37,14 @@ class ProductsController < ApplicationController
   end
   
   def move_to_index
+  end
+
+  def destroy
+    if @product.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
