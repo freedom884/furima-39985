437715aa239d8931082a0e1e302218775990fd9e
freedom884 <bass.ran.003@gmail.com>
2024-02-07 15:04:49 +0900

    リファクタリング

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_product, only: [:show,:edit,:update、:move_to_index]
+  before_action :require_same_user, only: [:edit, :move_to_index]
+
 
   def index 
     @products = Product.all.order(created_at: :desc)
@@ -19,18 +22,12 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
-    unless current_user.id == @product.user_id
-      redirect_to root_path
-    end
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path(@product)
     else
@@ -40,10 +37,6 @@ class ProductsController < ApplicationController
   end
   
   def move_to_index
-    @product = Product.find(params[:id])
-    unless current_user.id == @product.user_id
-      redirect_to root_path
-    end
   end
 
   private
@@ -52,6 +45,14 @@ class ProductsController < ApplicationController
     params.require(:product).permit(:name, :description, :image,:category_id ,:condition_id,:shippingfee_id,:prefecture_id,:scheduleddelivery_id,:price).merge(user_id: current_user.id,)
   end
 
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def require_same_user
+    unless current_user.id == @product.user_id
+      redirect_to root_path
+    end
  
 
 end
